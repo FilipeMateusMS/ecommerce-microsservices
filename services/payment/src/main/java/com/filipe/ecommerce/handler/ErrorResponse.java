1deab0eca26190344ad8e0ce4a33697cd68602ea
FilipeMateusMS <filipedesousademedeiros@gmail.com>
2026-06-12 16:17:05 -0300

    Corrigido templates e adicionado o manipulador de exceções no serviço Payment

---
+++ services/payment/src/main/java/com/filipe/ecommerce/handler/ErrorResponse.java
@@ -0,0 +1,9 @@
+package com.filipe.ecommerce.handler;
+
+import java.util.Map;
+
+public record ErrorResponse(
+        Map<String, String> errors
+) {
+
+}
--- services/payment/src/main/java/com/filipe/ecommerce/payment/PaymentRequest.java
+++ services/payment/src/main/java/com/filipe/ecommerce/payment/PaymentRequest.java
@@ -1,12 +1,24 @@
 package com.filipe.ecommerce.payment;
 
+import jakarta.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
 
 public record PaymentRequest(
         Long id,
+
+        @NotNull( message = "Field amount is required" )
         BigDecimal amount,
+
+        @NotNull( message = "Method of payment amount is required" )
         PaymentMethod paymentMethod,
+
+        @NotNull( message = "Order ID required" )
         Long orderId,
+
+        @NotNull( message = "Order reference is required")
         String orderReference,
+
+        @NotNull( message = "Customer is required" )
         Customer customer
 ) { }
